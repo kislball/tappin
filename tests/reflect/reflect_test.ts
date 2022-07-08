638aa3bf874ff8150da284770ea1b56f703fa7cd
Кислый Кислюк <kislball@yahoo.com>
2022-07-08 20:04:55 +0300

    fix tests

--- tests/reflect/reflect_test.ts
+++ tests/reflect/reflect_test.ts
@@ -82,7 +82,7 @@ Deno.test("gets all services with given metadata", async () => {
     return services.length;
   });
 
-  assertEquals(value, 1);
+  assertEquals(value, 2);
 });
 
 Deno.test("gets root module", async () => {
