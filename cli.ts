e208922b9dcaebaa8b3c026932ff5de350464c0d
Кислый Кислюк <kislball@yahoo.com>
2022-07-09 01:28:52 +0300

    pretty

--- cli.ts
+++ cli.ts
@@ -1,30 +1,31 @@
-import { latest } from './www/versions.ts'
+import { latest } from "./www/versions.ts";
 
-let dry = false
-let cmdName = Deno.args?.[0] ?? 'help'
+let dry = false;
+let cmdName = Deno.args?.[0] ?? "help";
 
-if (!['help', 'new', 'dry'].includes(cmdName)) cmdName = 'help'
+if (!["help", "new", "dry"].includes(cmdName)) cmdName = "help";
 
-const help = `tappin-init v${latest} - initialize Tappin project with one command
+const help =
+  `tappin-init v${latest} - initialize Tappin project with one command
 
 Commands:
   - new  - creates a new project in working directory
   - help - shows this message
-  - dry  - dry runs initializer`
+  - dry  - dry runs initializer`;
 
-if (cmdName === 'help') {
-  console.log(help)
-  Deno.exit()
-} else if (cmdName === 'dry') {
-  dry = true
+if (cmdName === "help") {
+  console.log(help);
+  Deno.exit();
+} else if (cmdName === "dry") {
+  dry = true;
 }
 
-const stringify = (a: any) => JSON.stringify(a, null, 2)
+const stringify = (a: any) => JSON.stringify(a, null, 2);
 
 const tappinJson = stringify({
   version: latest,
-  modules: ['core', 'dev'],
-})
+  modules: ["core", "dev"],
+});
 
 const mainTs = `import { start } from "$tappin/dev";
 
@@ -35,7 +36,7 @@ if (appName === undefined) {
 }
 
 await start(appName, import.meta.url);
-`
+`;
 
 const devTs = `import { generate, start } from "$tappin/dev";
 
@@ -49,37 +50,37 @@ if (appName === undefined) {
 }
 
 await start(appName, import.meta.url);
-`
+`;
 
 const files = {
-  'tappin.json': tappinJson,
-  'main.ts': mainTs,
-  'dev.ts': devTs,
-}
+  "tappin.json": tappinJson,
+  "main.ts": mainTs,
+  "dev.ts": devTs,
+};
 
 const dirs = [
-  'apps',
-  'libs',
-  'libs/dinosaurs-trivia',
-  'apps/dinosaurs',
-]
+  "apps",
+  "libs",
+  "libs/dinosaurs-trivia",
+  "apps/dinosaurs",
+];
 
-const content = Deno.readDirSync(".")
-if ([ ...content ].length !== 0) {
-  console.error('This directory is not empty. Empty it before usage of CLI.')
-  Deno.exit()
+const content = Deno.readDirSync(".");
+if ([...content].length !== 0) {
+  console.error("This directory is not empty. Empty it before usage of CLI.");
+  Deno.exit();
 }
 
 for (const dir of dirs) {
-  console.log(`Creating directory ${dir}`)
+  console.log(`Creating directory ${dir}`);
   if (!dry) {
-    Deno.mkdirSync(dir)
+    Deno.mkdirSync(dir);
   }
 }
 
 for (const entry of Object.entries(files)) {
-  console.log(`Creating file ${entry[0]}`)
+  console.log(`Creating file ${entry[0]}`);
   if (!dry) {
-    Deno.writeTextFileSync(entry[0], entry[1])
+    Deno.writeTextFileSync(entry[0], entry[1]);
   }
 }
--- dev/mod.ts
+++ dev/mod.ts
@@ -1,49 +1,53 @@
 /** Configuration for Tappin import map generaotr */
 export interface TappinJson {
-  version: string
-  modules: string[]
+  version: string;
+  modules: string[];
 }
 
 /** Generates import maps */
-export const generateImportMap = (projects: Record<string, string>, { version, modules }: TappinJson) => {
-  if (!modules.includes('dev')) modules.push('dev')
+export const generateImportMap = (
+  projects: Record<string, string>,
+  { version, modules }: TappinJson,
+) => {
+  if (!modules.includes("dev")) modules.push("dev");
   const map: { import_map: Record<string, string> } = {
-    import_map: {}
-  }
+    import_map: {},
+  };
 
   for (const entry of Object.entries(projects)) {
-    map.import_map[`$lib/${entry[0]}`] = entry[1]
+    map.import_map[`$lib/${entry[0]}`] = entry[1];
   }
 
   for (const module of modules) {
-    map.import_map[`$tappin/${module}`] = `https://deno.land/x/tappin@${version}/${module}/mod.ts`
+    map.import_map[`$tappin/${module}`] =
+      `https://deno.land/x/tappin@${version}/${module}/mod.ts`;
   }
 
-  return JSON.stringify(map, null, 2)
-}
+  return JSON.stringify(map, null, 2);
+};
 
 /** Generates import map from given JSON */
 export const generate = (base: string) => {
-  const tappinUrl = new URL('tappin.json', base).pathname
-  const data = Deno.readTextFileSync(tappinUrl)
-  const info: TappinJson = JSON.parse(data)
-  const libsDir = new URL('libs', base).pathname
-  const projects: Record<string, string> = {}
-  
+  const tappinUrl = new URL("tappin.json", base).pathname;
+  const data = Deno.readTextFileSync(tappinUrl);
+  const info: TappinJson = JSON.parse(data);
+  const libsDir = new URL("libs", base).pathname;
+  const projects: Record<string, string> = {};
+
   try {
-    const read = [ ...Deno.readDirSync(libsDir) ]
+    const read = [...Deno.readDirSync(libsDir)];
     for (const dir of read) {
       if (dir.isDirectory) {
-        projects[dir.name] = `./libs/${dir.name}/mod.ts`
+        projects[dir.name] = `./libs/${dir.name}/mod.ts`;
       }
     }
-  } catch {}
+  } catch { /* */ }
 
-  const r = generateImportMap(projects, info)
-  Deno.writeTextFileSync(tappinUrl, r)
-}
+  const r = generateImportMap(projects, info);
+  Deno.writeTextFileSync(tappinUrl, r);
+};
 
 /** Starts given application */
 export const start = async (appName: string, base: string) => {
-  await import(new URL(`apps/${appName}/main.ts`, base).href)
-}
+  await import(new URL(`apps/${appName}/main.ts`, base).href);
+};
