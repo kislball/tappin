3ba05cb131da792c2ccf292b793e384261bdf8e8
Кислый Кислюк <kislball@yahoo.com>
2022-07-09 00:07:10 +0300

    reset cli

--- cli.ts
+++ cli.ts
@@ -1,15 +1,1 @@
-import { cliModule } from "./cli/mod.ts";
-import {
-  CommandService,
-  commandService,
-} from "./cli/services/command-service.ts";
-import { createContainerHelper, createFactory } from "./core/mod.ts";
-
-const factory = createFactory(cliModule);
-
-await factory.init();
-
-const helper = createContainerHelper(factory.container());
-await helper.provide([commandService], (cmd: CommandService) => {
-  cmd.showHelp();
-});
+console.log('Hello, tappin!')
--- cli/mod.ts
+++
@@ -1,4 +0,0 @@
-import { createModule } from "../core/mod.ts";
-import { commandService } from "./services/command-service.ts";
-
-export const cliModule = createModule((dsl) => dsl.service(commandService));
