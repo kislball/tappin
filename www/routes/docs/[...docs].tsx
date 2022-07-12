5a09da761ce0af3b1db0d708e7032331ccc986c6
Кислый Кислюк <kislball@yahoo.com>
2022-07-12 19:01:35 +0300

    deploy

--- www/routes/docs/[...docs].tsx
+++ www/routes/docs/[...docs].tsx
@@ -21,7 +21,7 @@ export const handler: Handlers = {
     const path = resolvePath(doc);
 
     let markdownData: string;
-    console.log(path)
+    console.log(path, [...Deno.readDir('./docs')])
 
     try {
       markdownData = await Deno.readTextFile(path);
