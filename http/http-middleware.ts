7a49d854326219e40246cfe52cb92bc937387b60
Кислый Кислюк <kislball@yahoo.com>
2022-07-22 12:24:43 +0300

    feat(http): add middleware typings

--- http/http-middleware.ts
+++ http/http-middleware.ts
@@ -5,7 +5,7 @@ export const serialize = (a: any) =>
   a instanceof Response ? a : new Response(a);
 
 /** Represents a middleware */
-export type Middleware = (ctx: Context, next: () => any) => any;
+export type Middleware<T = any, R = any> = (ctx: Context<T>, next: () => any) => Promise<R> | R;
 
 /**
  * Combines middlewares into a single function and returns a function
