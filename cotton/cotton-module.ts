38ee5fde189f948eb1718fcb9a0b99ca49315c09
Кислый Кислюк <kislball@yahoo.com>
2022-07-31 20:05:05 +0300

    feat(cotton)!: remove cotton

--- cotton/cotton-module.ts
+++
@@ -1,8 +0,0 @@
-import { createModule } from "../core/module.ts";
-import { cottonService } from "./cotton-service.ts";
-
-export const cottonModule = createModule((dsl) =>
-  dsl
-    .name("CottonModule")
-    .service(cottonService)
-);
--- cotton/mod.ts
+++
@@ -1,4 +0,0 @@
-export * from "./cotton-mixin.ts";
-export * from "./cotton-module.ts";
-export * from "./cotton-options.ts";
-export * from "./cotton-service.ts";
